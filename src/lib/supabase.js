4b390fd820481185031d0bbc2478a8b13154d0f3
sanacoder26 <wcoding819@gmail.com>
2026-02-15 13:25:56 +0500

    commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,14 @@
 
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = '';
-const supabaseAnonKey = '';
+// const supabaseUrl = '';
+// const supabaseAnonKey = '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
